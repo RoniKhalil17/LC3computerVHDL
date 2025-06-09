c353c6cc9548d4847af266a440798efb65a97bea
Jacob Benedictus Søgaard Nielsen <jacob.b.soegaard@gmail.com>
2025-06-09 15:09:56 +0200

    Update playerCreat.asm
    start på player creation

--- Assembly/Subroutines/playerCreat.asm
+++ Assembly/Subroutines/playerCreat.asm
@@ -1,1 +1,13 @@
-
+.orig x3000
+LEA R1, player1 ;loader adressen til arrayet ind
+and r2, r2, #0 ; sikker at den er nul
+add r2, r2, #3  ;player navn bliver max på 3 bogstaver
+loop        ; loop
+getc        ;til at få bogstav
+str r0, r1, #0  ; gemmer bogstavet
+add r1, r1, #1  ;rykker en plads
+add r2, r2, #-1 ;-1 så man kommer ud af loopet på det rigtige tidspunkt.
+brnp loop   ;loop
+halt    ;er ikke done men det er en start
+player1  .blkw #3 
+.end
