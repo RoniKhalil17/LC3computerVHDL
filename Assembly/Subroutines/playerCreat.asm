895256d6d66e078d550bd281a9b37d5e9513d309
Jacob Benedictus Søgaard Nielsen <jacob.b.soegaard@gmail.com>
2025-06-10 11:09:55 +0200

    Update playerCreat.asm
    det funger nu, med max 3 spiller, der skal trykkes nul nå nok spiller er med

--- Assembly/Subroutines/playerCreat.asm
+++ Assembly/Subroutines/playerCreat.asm
@@ -1,6 +1,7 @@
 .orig x3000
+add r3, r3, #3
 LEA R1, player1 ;loader adressen til arrayet ind
-and r2, r2, #0 ; sikker at den er nul
+nySpiller and r2, r2, #0 ; sikker at den er nul
 add r2, r2, #3  ;player navn bliver max på 3 bogstaver
 loop        ; loop
 getc        ;til at få bogstav
@@ -11,11 +12,20 @@ brnp loop   ;loop
 add r1, r1, #1  ;rykker en plads for at gør plads til null termintor
 LD  r0, startMoney
 str r0, r1, #0  ; slut bogstavet
-LEA R0, player1 ;loader adressen til arrayet ind
-puts 
+
+add r1, r1, #12 ; næste array
+;str r0, r1, #0  ; checeker at den er kommet til næste array.
+getc    ; hvis 0 så er alle spiller med.
+ld  r4 tiltal   ; laver char til tal
+add r0, r0, r4  ; laver char til tal
+brz done        ; hvis 0 bliver tastet ind så er alle spiller med.
+add r3, r3, #-1
+brp nySpiller
+done
 halt    ;er ikke done men det er en start
 startMoney .fill #300   ;er bare et eksempel på man kan start med penge
-player1  .blkw #14
-player2  .blkw #14
-player3  .blkw #14
+tilTal       .fill #-48
+player1  .blkw #16  ; indholder 4 til navn, 1 til holde score, 10 til at indholde historik en til null termintor og buffer.
+player2  .blkw #16
+player3  .blkw #16
 .end
