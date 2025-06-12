b824b0871743dbc944f34f6686f507dd3594c95e
RoniKhalil17 <s245495@student.dtu.dk>
2025-06-12 11:13:04 +0200

    Update FIFO.vhd

--- UART/FIFO.vhd
+++ UART/FIFO.vhd
@@ -1,91 +1,109 @@
--- Listing 7.1
+-- Listing 4.20
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-entity uart_rx is
+entity fifo is
    generic(
-      DBIT: integer:=8;     -- # data bits
-      SB_TICK: integer:=16  -- # ticks for stop bits
+      B: natural:=8; -- number of bits
+      W: natural:=4 -- number of address bits
    );
    port(
       clk, reset: in std_logic;
-      rx: in std_logic;
-      s_tick: in std_logic;
-      rx_done_tick: out std_logic;
-      d_out: out std_logic_vector(7 downto 0)
+      rd, wr: in std_logic;
+      w_data: in std_logic_vector (B-1 downto 0);
+      empty, full: out std_logic;
+      r_data: out std_logic_vector (B-1 downto 0)
    );
-end uart_rx ;
+end fifo;
 
-architecture arch of uart_rx is
-   type state_type is (idle, start, data, stop);
-   signal state_reg, state_next: state_type;
-   signal s_reg, s_next: unsigned(3 downto 0);
-   signal n_reg, n_next: unsigned(2 downto 0);
-   signal b_reg, b_next: std_logic_vector(7 downto 0);
+architecture arch of fifo is
+   type reg_file_type is array (2**W-1 downto 0) of
+        std_logic_vector(B-1 downto 0);
+   signal array_reg: reg_file_type;
+   signal w_ptr_reg, w_ptr_next, w_ptr_succ:
+      std_logic_vector(W-1 downto 0);
+   signal r_ptr_reg, r_ptr_next, r_ptr_succ:
+      std_logic_vector(W-1 downto 0);
+   signal full_reg, empty_reg, full_next, empty_next:
+          std_logic;
+   signal wr_op: std_logic_vector(1 downto 0);
+   signal wr_en: std_logic;
 begin
-   -- FSMD state & data registers
+   --=================================================
+   -- register file
+   --=================================================
    process(clk,reset)
    begin
-      if reset='1' then
-         state_reg <= idle;
-         s_reg <= (others=>'0');
-         n_reg <= (others=>'0');
-         b_reg <= (others=>'0');
+     if (reset='1') then
+        array_reg <= (others=>(others=>'0'));
+     elsif (clk'event and clk='1') then
+        if wr_en='1' then
+           array_reg(to_integer(unsigned(w_ptr_reg)))
+                 <= w_data;
+        end if;
+     end if;
+   end process;
+   -- read port
+   r_data <= array_reg(to_integer(unsigned(r_ptr_reg)));
+   -- write enabled only when FIFO is not full
+   wr_en <= wr and (not full_reg);
+
+   --=================================================
+   -- fifo control logic
+   --=================================================
+   -- register for read and write pointers
+   process(clk,reset)
+   begin
+      if (reset='1') then
+         w_ptr_reg <= (others=>'0');
+         r_ptr_reg <= (others=>'0');
+         full_reg <= '0';
+         empty_reg <= '1';
       elsif (clk'event and clk='1') then
-         state_reg <= state_next;
-         s_reg <= s_next;
-         n_reg <= n_next;
-         b_reg <= b_next;
+         w_ptr_reg <= w_ptr_next;
+         r_ptr_reg <= r_ptr_next;
+         full_reg <= full_next;
+         empty_reg <= empty_next;
       end if;
    end process;
-   -- next-state logic & data path functional units/routing
-   process(state_reg,s_reg,n_reg,b_reg,s_tick,rx)
+
+   -- successive pointer values
+   w_ptr_succ <= std_logic_vector(unsigned(w_ptr_reg)+1);
+   r_ptr_succ <= std_logic_vector(unsigned(r_ptr_reg)+1);
+
+   -- next-state logic for read and write pointers
+   wr_op <= wr & rd;
+   process(w_ptr_reg,w_ptr_succ,r_ptr_reg,r_ptr_succ,wr_op,
+           empty_reg,full_reg)
    begin
-      state_next <= state_reg;
-      s_next <= s_reg;
-      n_next <= n_reg;
-      b_next <= b_reg;
-      rx_done_tick <='0';
-      case state_reg is
-         when idle =>
-            if rx='0' then
-               state_next <= start;
-               s_next <= (others=>'0');
-            end if;
-         when start =>
-            if (s_tick = '1') then
-               if s_reg=7 then
-                  state_next <= data;
-                  s_next <= (others=>'0');
-                  n_next <= (others=>'0');
-               else
-                  s_next <= s_reg + 1;
+      w_ptr_next <= w_ptr_reg;
+      r_ptr_next <= r_ptr_reg;
+      full_next <= full_reg;
+      empty_next <= empty_reg;
+      case wr_op is
+         when "00" => -- no op
+         when "01" => -- read
+            if (empty_reg /= '1') then -- not empty
+               r_ptr_next <= r_ptr_succ;
+               full_next <= '0';
+               if (r_ptr_succ=w_ptr_reg) then
+                  empty_next <='1';
                end if;
             end if;
-         when data =>
-            if (s_tick = '1') then
-               if s_reg=15 then
-                  s_next <= (others=>'0');
-                  b_next <= rx & b_reg(7 downto 1) ;
-                  if n_reg=(DBIT-1) then
-                     state_next <= stop ;
-                  else
-                     n_next <= n_reg + 1;
-                  end if;
-               else
-                  s_next <= s_reg + 1;
+         when "10" => -- write
+            if (full_reg /= '1') then -- not full
+               w_ptr_next <= w_ptr_succ;
+               empty_next <= '0';
+               if (w_ptr_succ=r_ptr_reg) then
+                  full_next <='1';
                end if;
             end if;
-         when stop =>
-            if (s_tick = '1') then
-               if s_reg=(SB_TICK-1) then
-                  state_next <= idle;
-                  rx_done_tick <='1';
-               else
-                  s_next <= s_reg + 1;
-               end if;
-            end if;
+         when others => -- write/read;
+            w_ptr_next <= w_ptr_succ;
+            r_ptr_next <= r_ptr_succ;
       end case;
    end process;
-   d_out <= b_reg;
+   -- output
+   full <= full_reg;
+   empty <= empty_reg;
 end arch;
